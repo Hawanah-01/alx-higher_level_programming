b485d1e1e4810b18d301ba315d7dcacb0242faae
Hawanah-01 <olawaleawanat@gmail.com>
2025-02-27 16:00:05 +0100

    Task 3-When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) == 'q' and chr(i) == 'e':
-        continue
-    print(chr(i).format(i), end='')
+    if chr(i) != "e"  and chr(i) != "q" :
+        print('{:c}'.format(i), end='')
